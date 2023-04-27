b876799c925d93e6441c40eacbb756e6e70eaf6f
Astha Sachan <astha.sachan@o9solutions.com>
2023-04-27 16:20:04 +0530

    test changes

--- src/exception.py
+++ src/exception.py
@@ -19,8 +19,8 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message    
-'''    
-
+   
+'''
 if __name__=="__main__":
     logging.info("Logging has started")
     try:
